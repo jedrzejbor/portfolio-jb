3d19838df5420f6343488885377d950e9ab302ec
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-07-09 23:12:55 +0200

    Add box under text

--- src/views/Head.js
+++ src/views/Head.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import Button from '../components/atoms/Button/Button';
 
-const Head = () => {
 
+const Head = () => {
+ const Square = styled.span`
+ display:inline-block;
+ /* height: 20px; */
+ /* width: 20px; */
+  background-color: #9815FF;
+  color: white;
+ `;
   return ( 
    <div>
-  <h1> Hi, <br/>I’m Jędrzej Borakiewicz,
+  <h1><Square>Hi, <br/>I’m Jędrzej</Square> Borakiewicz,
     web developer. </h1>
     <Button>About</Button>
     </div>
