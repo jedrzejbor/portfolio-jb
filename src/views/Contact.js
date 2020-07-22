2f4b3f19287ef3081dd0050118f276e87ec2f010
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-07-22 09:08:23 +0200

    Reform Contact section moblie look

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import TopicTitle from '../components/atoms/TopicTitle/TopicTitle';
+import Button from '../components/atoms/Button/Button';
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
 `;
 
+const WrapperText = styled.div`
+  height: 50vh;
+  width: 75vw;
+  margin: 0 auto;
+`;
+
+const GrayText = styled.p`
+  color: #828282;
+  font-weight: lighter;
+  font-size: 1.6rem;
+  padding: 5px 0;
+`;
+
+const ButtonCv = styled(Button)`
+  display: block;
+  margin: 20px auto;
+`;
+
 const Contact = () => {
   return (
     <Wrapper>
-      <TopicTitle>Contact</TopicTitle>
-      <h3>Email: jedrzej.borakiewicz@gmail.com</h3>
+      <WrapperText>
+        <TopicTitle>Contact</TopicTitle>
+        <h3>EMAIL</h3>
+        <GrayText>jedrzej.borakiewicz@gmail.com</GrayText>
+        <h3>GITHUB</h3>
+        <GrayText>jedbor</GrayText>
+        <h3>LINKEDIN</h3>
+        <GrayText>Jędrzej Borakiewicz</GrayText>
+        <ButtonCv>DOWNLOAD CV</ButtonCv>
+      </WrapperText>
     </Wrapper>
   );
 };
