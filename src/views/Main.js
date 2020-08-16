c1e710542ee31bb29b43693d28aaaa1e742213af
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-08-16 22:34:46 +0200

    Delete warnings

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import MobileMenu from '../components/organisms/MobileMenu/MobileMenu';
 import Head from './Head';
